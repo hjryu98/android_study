ae9b07c59ac47e9d0711ce4d6d0c4aeb0c7343a9
hjryu98 <nha98@naver.com>
2025-09-06 17:06:59 +0900

    Linear, Relative, FrameLayout

---
+++ Layout_Study/app/src/main/java/com/example/layout_study/FrameActivity.kt
@@ -0,0 +1,21 @@
+package com.example.layout_study
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// view binding 구현 필요.
+class FrameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.framelayout_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.frame)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
